495199f701b78fb2fa8c8acf133806a16526f0c4
YutaKonishi <cony0413@gmail.com>
2018-04-24 18:48:43 +0900

    Q7.sqlまで完了

---
+++ Q3.sql
@@ -0,0 +1,7 @@
+INSERT INTO item_category(category_name) VALUES('‰Æ‹ï');
+Query OK, 1 row affected (0.07 sec)
+
+mysql> INSERT INTO item_category(category_name) VALUES('H•i');
+Query OK, 1 row affected (0.08 sec)
+
+mysql> INSERT INTO item_category(category_name) VALUES('–{');
---
+++ Q4.sql
@@ -0,0 +1,45 @@
+INSERT INTO item(item_name,item_price,category_id) VALUES('堅牢な机',3000,1);
+Query OK, 1 row affected (0.03 sec)
+
+mysql> INSERT INTO item(item_name,item_price,category_id) VALUES('生焼け肉',3000,1);
+Query OK, 1 row affected (0.06 sec)
+
+mysql> INSERT INTO item(item_name,item_price,category_id) VALUES('すっきりわかるJava入門',3000,1);
+Query OK, 1 row affected (0.06 sec)
+
+mysql> INSERT INTO item(item_name,item_price,category_id) VALUES('おしゃれな椅子',3000,1);
+Query OK, 1 row affected (0.06 sec)
+
+mysql> INSERT INTO item(item_name,item_price,category_id) VALUES('こんがり肉',3000,1);
+Query OK, 1 row affected (0.08 sec)
+
+mysql> INSERT INTO item(item_name,item_price,category_id) VALUES('書き方ドリルSQL',3000,1);
+Query OK, 1 row affected (0.03 sec)
+
+mysql> INSERT INTO item(item_name,item_price,category_id) VALUES('ふわふわのベッド',3000,1);
+Query OK, 1 row affected (0.06 sec)
+
+mysql> INSERT INTO item(item_name,item_price,category_id) VALUES('ミラノ風ドリア',300,2);
+Query OK, 1 row affected (0.06 sec)
+
+mysql> UPDATE item SET item_price = 30000 ,category_id = 1 WHERE item_id = 7;
+Query OK, 1 row affected (0.08 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> UPDATE item SET item_price = 2500 ,category_id = 3 WHERE item_id = 6;
+Query OK, 1 row affected (0.08 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> UPDATE item SET item_price = 500 ,category_id = 2 WHERE item_id = 5;
+Query OK, 1 row affected (0.06 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> UPDATE item SET item_price = 2000 ,category_id = 1 WHERE item_id = 4;
+Query OK, 1 row affected (0.08 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> UPDATE item SET item_price = 3000 ,category_id = 3 WHERE item_id = 3;
+Query OK, 1 row affected (0.08 sec)
+Rows matched: 1  Changed: 1  Warnings: 0
+
+mysql> UPDATE item SET item_price = 50 ,category_id = 2 WHERE item_id = 2;
