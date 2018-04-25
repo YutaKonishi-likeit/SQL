cbea0a45ae59b46fdc886ff215eaed0601282bbe
YutaKonishi <cony0413@gmail.com>
2018-04-25 11:04:43 +0900

    Q9.sqlまで完了

---
+++ Q8.sql
@@ -0,0 +1,1 @@
+SELECT item.item_id, item_name, item_price, item_category.category_name FROM item INNER JOIN item_category ON item.category_id = item_category.category_id;
---
+++ Q9.sql
@@ -0,0 +1,1 @@
+SELECT C.category_name, SUM(I.item_price) AS total_price FROM item I INNER JOIN item_category C ON I.category_id = C.category_id GROUP BY I.category_id ORDER BY total_price DESC;
